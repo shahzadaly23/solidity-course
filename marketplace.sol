9f69fc9222ef9e649a53dd77a435c8f1bf52b9f6
shahzadaly23 <shahzad.aly23@gmail.com>
2023-08-29 22:24:16 +0500

    a

--- marketplace.sol
+++ marketplace.sol
@@ -17,12 +17,16 @@ contract marketplace{
     emit purchase(buyer, sender, C_P );
 }    
     error custom(string reason);
-    function sell(address sender, address buyer, uint S_P) public pure virtual  returns(string memory){
+    function sell(address sender, address buyer, uint S_P) public virtual  returns(string memory){
+    require(S_P != 0);
     if (balance[sender] == 0){
         revert custom( "seller is not registered");
     }
     else if (balance[buyer] == 0){
         revert custom ("buyer is not registered");
+    }
+    else if(balance[sender] == balance[buyer]){
+        revert custom ("seller and buyer cant be one person");
     }
     else if (balance[sender] == 0 && balance[buyer] == 0){
         revert custom ("seller and buyer both are not registered");
@@ -37,13 +41,14 @@ contract PremiumSeller is marketplace{
     constructor(uint _feepercentage){
      feepercentage = _feepercentage;
     }
-    function sell(address sender, address buyer, uint S_P) public pure  virtual override returns(string memory) {
+    function sell(address sender, address buyer, uint S_P) public virtual override returns(string memory) {
     require(balance[sender] == 0, "your balance is zero");
     uint fee = S_P * feepercentage / 100;
     balance[sender] -= fee;
     balance[address(this)] += fee;
 
     super.sell(sender,buyer,S_P);
+    return ("item will be sent to your address");
     }
 
 }
@@ -52,14 +57,22 @@ contract RegularBuyer is marketplace{
 }
 contract VIP_buyer is marketplace{
     uint public discount;
+    uint public finalAmount;
     constructor(uint dispercentage){
-        
-        discount= dispercentage/ 100;
+        discount = dispercentage;
     }
 
-    function buy() public virtual override {
+    function buy(address sender, address buyer, uint C_P) public virtual override {
+        require(balance[sender] != 0," seller is not registered");
+        require(balance[buyer] != 0," buyer is not registered");
+        require(C_P != 0, " cost cant be zero");
+
+        finalAmount = C_P * discount/100; 
+        balance[sender] += finalAmount;
+        balance[buyer] -= finalAmount; 
+
+        emit purchase(buyer, sender, finalAmount);
        
-    uint sell_price = 89; 
 
     }
 
