e4c13da62fde2e0fd3c5c3e593e277a4fddc1a4b
shahzadaly23 <shahzad.aly23@gmail.com>
2023-08-30 14:31:30 +0500

    a

--- MyToken.sol
+++ MyToken.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MyToken is ERC20, ERC20Burnable, Ownable {
-    constructor() ERC20("murtaza", "gmk") {}
+    constructor() ERC20("Shahzad", "SHA") {}
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
